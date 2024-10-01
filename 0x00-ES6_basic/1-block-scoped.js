b6799f25b9deca2cac239f8b1e52b778153947b8
simoZak <zakhnini92@gmail.com>
2024-10-01 01:44:26 +0100

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -8,14 +8,4 @@ export default function taskBlock(trueOrFalse) {
   }
 
   return [task, task2];
-}iexport default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
-  }
-
-  return [task, task2];
 }
